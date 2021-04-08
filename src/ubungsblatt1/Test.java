8b4fe2b0e7b8eb1499ee488b4f291d96959eb7bb
CookiesPlays <82169964+CookiesPlays@users.noreply.github.com>
2021-04-08 19:53:53 +0200

    New line

--- src/ubungsblatt1/Test.java
+++ src/ubungsblatt1/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	  public static void main(String[] args) {
 		    System.out.println("This does work");
 		    System.out.println("doesnt it?");
+		    System.out.println("I think it does");
 	  }
 }

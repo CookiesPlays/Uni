421d830207fed0c18dfd4ea323aea3a6d576db2c
CookiesPlays <82169964+CookiesPlays@users.noreply.github.com>
2021-04-08 19:49:17 +0200

    testchanged

--- src/ubungsblatt1/Test.java
+++ src/ubungsblatt1/Test.java
@@ -2,7 +2,7 @@ package ubungsblatt1;
 
 public class Test {
 	  public static void main(String[] args) {
-		    System.out.println("Git is fun");
-		    System.out.println("isnt it?");
+		    System.out.println("This does work");
+		    System.out.println("doesnt it?");
 	  }
 }
